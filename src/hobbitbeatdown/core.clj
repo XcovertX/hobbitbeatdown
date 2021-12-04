a15de8fcec99c636b7559610ce3200fed2e9f974
James Covert <jcovert2@asu.edu>
2021-12-04 01:13:27 -0800

    symmetrize body function added

--- src/hobbitbeatdown/core.clj
+++ src/hobbitbeatdown/core.clj
@@ -2,6 +2,33 @@
   (:gen-class))
 
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+  [& args])
+
+(def asym-hobbit-body-parts [{:name "head" :size 3}
+                             {:name "left-eye" :size 1}
+                             {:name "left-ear" :size 1}
+                             {:name "mouth" :size 1}
+                             {:name "nose" :size 1}
+                             {:name "neck" :size 2}
+                             {:name "chest" :size 8}
+                             {:name "downstairs" :size 1}
+                             {:name "left-leg" :size 3}
+                             {:name "left-arm" :size 3}])
+
+(defn matching-part
+  [part]
+  {:name (clojure.string/replace (:name part) #"^left-" "right-")
+   :size (:size part)})
+
+(defn symmetrize-body-parts
+  "Expects a seq of maps that have a :name and :size"
+  [asym-body-parts]
+  (loop [remaining-asym-parts asym-body-parts
+         final-body-parts []]
+    (if (empty? remaining-asym-parts)
+      final-body-parts
+      (let [[part & remaining] remaining-asym-parts]
+        (recur remaining
+               (into final-body-parts
+                     (set [part (matching-part part)])))))))
+  
